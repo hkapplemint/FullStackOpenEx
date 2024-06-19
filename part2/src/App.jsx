45b737fdcf5683838b236167d4de286d5e32c780
hkapplemint <laitatwai@gmail.com>
2024-06-19 14:52:57 +0800

    2.10

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 
-const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456", id: 1 },
-        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    ]);
+const Filter = ({ persons }) => {
+    const [search, setSearch] = useState("");
+
+    const handleSearch = event => {
+        setSearch(event.target.value);
+    };
+
+    return (
+        <div>
+            filter shown with <input onChange={handleSearch} value={search} />
+            {search === "" ? (
+                <></>
+            ) : (
+                persons
+                    .filter(person => person.name.toLowerCase().includes(search))
+                    .map(person => (
+                        <p key={person.name}>
+                            {person.name} {person.number}
+                        </p>
+                    ))
+            )}
+        </div>
+    );
+};
+
+const PersonForm = ({ persons, setPersons }) => {
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
-    const [search, setSearch] = useState("");
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -35,47 +53,51 @@ const App = () => {
         setNewNumber(event.target.value);
     };
 
-    const handleSearch = event => {
-        setSearch(event.target.value);
-    };
-
     return (
-        <div>
-            <h2>Phonebook</h2>
+        <form>
             <div>
-                filter shown with <input onChange={handleSearch} value={search} />
-                {search === "" ? (
-                    <></>
-                ) : (
-                    persons
-                        .filter(person => person.name.toLowerCase().includes(search))
-                        .map(person => (
-                            <p key={person.name}>
-                                {person.name} {person.number}
-                            </p>
-                        ))
-                )}
+                name: <input onChange={handleNewName} value={newName} />
             </div>
-            <form>
-                <h2>add a new</h2>
-                <div>
-                    name: <input onChange={handleNewName} value={newName} />
-                </div>
-                <div>
-                    number: <input onChange={handleNewNumber} value={newNumber} />
-                </div>
-                <div>
-                    <button type="submit" onClick={handleSubmit}>
-                        add
-                    </button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
+            <div>
+                number: <input onChange={handleNewNumber} value={newNumber} />
+            </div>
+            <div>
+                <button type="submit" onClick={handleSubmit}>
+                    add
+                </button>
+            </div>
+        </form>
+    );
+};
+
+const Persons = ({ persons }) => {
+    return (
+        <>
             {persons.map(person => (
                 <p key={person.name}>
                     {person.name} {person.number}
                 </p>
             ))}
+        </>
+    );
+};
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ]);
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter persons={persons} />
+            <h3>Add a new</h3>
+            <PersonForm persons={persons} setPersons={setPersons} />
+            <h3>Numbers</h3>
+            <Persons persons={persons} />
         </div>
     );
 };
